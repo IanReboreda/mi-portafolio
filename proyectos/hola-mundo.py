f3389b99d7704dd3063af7e41a6035e643e5afa7
Ian Reboreda <ian.reboreda@hotmail.com>
2026-01-10 21:00:49 -0600

    feat: Añade primer script Python interactivo

--- proyectos/hola-mundo.py
+++ proyectos/hola-mundo.py
@@ -1,1 +1,3 @@
-print("¡Hola desde mi portafolio!")
+nombre = input("¿Cómo te llamas? ")
+print(f"Hola {nombre}, bienvenido a mi portafolio GitHub")
+print("Soy Ian Reboreda, estudiante de Ingeniería en Sistemas")
